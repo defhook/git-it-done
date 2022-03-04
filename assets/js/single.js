f19d03bd0cd07438519e1d66b0152f35b7ae9a69
Alvin Badillo <defhook152@gmail.com>
2022-03-04 06:56:58 -0700

    read and use data form query parameter

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -23,6 +24,14 @@ var getRepoIssues = function (repo) {
         }
     });
 };
+
+var getRepoName = function(){
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+
+}
 
 var displayIssues = function (issues) {
     if (issues.length === 0) {
@@ -76,4 +85,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
   };
 
-getRepoIssues("facebook/react");
+getRepoName();
